78cf7e95fcda329faccb7fd9d9c2c71b8bd3775f
Jean <jeanvictorgithubprojects@gmail.com>
2023-02-17 19:38:59 -0300

    feat: split reducers and combine reducers in rootReducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,54 +1,10 @@
-const initialState = {
-    todos: [
-        {id: 0, text: 'Learn React', completed: true },
-        {id: 1, text: 'Learn Redux', completed: false, color: 'purple'},
-        {id: 2, text: 'Build something fun!', completed: false, color: 'blue'}
-    ],
-    filters: {
-        status: 'All',
-        colors: []
-    }   
-}
+import todosReducer from "./features/todos/todosSlice"
+import filtersReducer from "./features/filters/filtersSlice"
+import { combineReducers } from "redux"
 
-function nextTodoId(todos) {
-    const maxId = todos.reduce((maxId, todo) => Math.max(maxId, todo.id),-1)
-    return maxId + 1
-}
+const rootReducer = combineReducers({
+    todos: todosReducer,
+    filters: filtersReducer
+})
 
-export default function appReducer(state = initialState, action){
-    switch(action.type){
-        case 'todos/todoAdded': 
-            return {
-                ...state,
-                todos: [
-                    ...state.todos,
-                    {
-                        id: nextTodoId(state.todos),
-                        text: action.payload,
-                        completed: false
-                    }
-                ]
-            }
-        case 'todos/todoToggled': 
-            return {
-                ...state,
-                todos: state.todos.filter((todo) => {
-                    if(todo.id !== action.payload){
-                        return todo
-                    }
-                    return {
-                        ...todo,
-                        completed:!todo.completed
-                    }
-                })
-            }
-        
-        case 'filters/statusFilterChanged':
-            return {
-                ...state.filters,
-                status: action.payload
-            } 
-        default: 
-            return state 
-    }
-}
+export default rootReducer
